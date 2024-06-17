3c90fd373360934db940707f971fee582072ca18
Aarsh-V <aarsh.vishwakarma.cd.mec22@itbhu.ac.in>
2024-06-17 17:15:42 +0530

    Add files via upload
    App and index javascript files

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Resturant from './component/basic/Resturant'
+// import UseState from './component/Hooks/useState'
+// import UseEffect from './component/Hooks/useEffect'
+// import Todo from './component/todoreact/todo'
+import Temp from './component/weather/temp'
+const App = () => {
+  return (
+    <>
+    <Temp/>
+    </ >
+  )
+}
+
+export default App
